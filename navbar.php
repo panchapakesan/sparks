81d017c273d485f1fb8ed5190e2a67df018359ef
panchapakesan <87406834+panchapakesan@users.noreply.github.com>
2021-08-21 15:54:14 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- navbar --> 
+      
+      <nav class="navbar navbar-light" style="background-color: #afff03;">
+      <a class="navbar-brand" href="index.php">KARUR VYSYA BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
